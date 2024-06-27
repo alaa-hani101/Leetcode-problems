22aea367a62d55a9c49b3ada2bfb0bc403bcd1ed
Alaa Hani <118071907+alaa-hani101@users.noreply.github.com>
2024-06-27 20:45:03 +0300

    Array & Hash table

---
+++ Tow Sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+         
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        unordered_map<int, int> mp; // val -> index
+        for (int i=0;i<nums.size();i++)
+        {
+             int tmp =target-nums[i];
+            if(mp.find(tmp) != mp.end())
+            {
+                return {mp[tmp],i};
+            }
+            mp.insert({nums[i],i});
+        }
+           return {};
+    }
+};
